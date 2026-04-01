f21a5167e06216cdd9d873adad5a80313fa6fbfd
cbatec <57846208+cbatec@users.noreply.github.com>
2026-04-01 18:36:12 +0100

    Create __manifest__.py

---
+++ erp_branding/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "ERP Branding",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Custom ERP Branding",
+    "depends": ["web"],
+    "data": [
+        "views/webclient.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
